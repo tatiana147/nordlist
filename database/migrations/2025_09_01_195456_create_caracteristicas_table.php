a23ab42fd5a894ec779e4d37196803970c13ff1d
tatiana147 <tatiana3148978684@gmail.com>
2025-09-02 14:41:44 -0500

    Primer commit

---
+++ database/migrations/2025_09_01_195456_create_caracteristicas_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('caracteristicas', function (Blueprint $table) {
+            $table->id();
+             $table->string('nombre',60);
+              $table->string('descripcion',225)->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('caracteristicas');
+    }
+};
